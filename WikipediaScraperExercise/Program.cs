873334cb19cc3e985aa6079fb04587641222e559
Matteo Sicilia <150261032+Matteo-Sicilia@users.noreply.github.com>
2024-02-12 11:43:56 +0100

    primo

---
+++ WikipediaScraperExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace WikipediaScraperExercise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (WebClient client = new WebClient())
+        {
+            string googleMainPage = client.DownloadString("https://www.google.com");
+            Console.WriteLine(googleMainPage);
+        }
+    }
+}
